043a03dc79bb2c15d9494ac0e13fcfcce5dc3e5b
KOUSHIK REEDY <2300031368@kluniversity.in>
2026-09-07 23:41:41 +0530

    revert: remove Amazon/Criteo/Intercom patterns (pending pattern review)

--- backend/services/trackerScanner.js
+++ backend/services/trackerScanner.js
@@ -55,18 +55,6 @@ function scanTrackers(html) {
             name: "Microsoft Clarity",
             pattern: /clarity\.ms/i
         },
-        {
-            name: "Amazon Ads",
-            pattern: /amazon-adsystem\.com|amzn\.to/i
-        },
-        {
-            name: "Criteo",
-            pattern: /criteo\.com|criteo\.net/i
-        },
-        {
-            name: "Intercom",
-            pattern: /intercom\.io|widget\.intercom\.io/i
-        }
     ];
 
     trackerPatterns.forEach((tracker) => {
